5122e6e3dfab3b0729e837c1d53a1133a0faefe4
Kulpot <kulpotwahu@gmail.com>
2024-03-09 00:32:55 +0800

    Board myBoard = new Board(8);

--- C# Chess Board 11/Form1.cs
+++ C# Chess Board 11/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassBoardModel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,10 +14,19 @@ using System.Windows.Forms;
 
 // add label(Prop:Text:Select a piece from the drop down menu.....), comboBox(Prop:items->...->Bishop/Rook...), Panel(Prop:SizeWidth500Height500),Form1(Prop:Text:Chess Piece Moves)
 
+//--------------------------- C# Chess Board 11 placebuttons ---------------------------------------------
+//ref link:https://www.youtube.com/watch?v=f3QRYl3o5Ak&list=PLhPyEFL5u-i0YDRW6FLMd1PavZp9RcYdF&index=11
+
+//
+
 namespace C__Chess_Board_10
 {
     public partial class Form1 : Form
     {
+        //------------START------------ C# Chess Board 11 placebuttons ---------------------------------------------
+        Board myBoard = new Board(8);
+        //-------------END------------ C# Chess Board 11 placebuttons ---------------------------------------------
+
         public Form1()
         {
             InitializeComponent();
