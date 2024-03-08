14a1f6cf00aad7842df69505ee2b427d9bc89fde
Kulpot <kulpotwahu@gmail.com>
2024-03-09 00:23:48 +0800

    c/p

--- C# Chess Board 11/Form1.cs
+++ C# Chess Board 11/Form1.cs
@@ -8,7 +8,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace C__Chess_Board_11
+//--------------------------- C# Chess Board 10 gui controls -------------------------------------------------
+//ref link:https://www.youtube.com/watch?v=cTNtnS6BSN0&list=PLhPyEFL5u-i0YDRW6FLMd1PavZp9RcYdF&index=10&t=1s
+
+// add label(Prop:Text:Select a piece from the drop down menu.....), comboBox(Prop:items->...->Bishop/Rook...), Panel(Prop:SizeWidth500Height500),Form1(Prop:Text:Chess Piece Moves)
+
+namespace C__Chess_Board_10
 {
     public partial class Form1 : Form
     {
