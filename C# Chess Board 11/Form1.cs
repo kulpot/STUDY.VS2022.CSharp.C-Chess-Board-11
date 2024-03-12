427f1a5e0dca8e440d8a3725850a98ea2bbb073c
Kulpot <kulpotwahu@gmail.com>
2024-03-12 21:45:11 +0800

    end

--- C# Chess Board 11/Form1.cs
+++ C# Chess Board 11/Form1.cs
@@ -12,7 +12,7 @@ using System.Windows.Forms;
 //--------------------------- C# Chess Board 10 gui controls -------------------------------------------------
 //ref link:https://www.youtube.com/watch?v=cTNtnS6BSN0&list=PLhPyEFL5u-i0YDRW6FLMd1PavZp9RcYdF&index=10&t=1s
 
-// add label(Prop:Text:Select a piece from the drop down menu.....), comboBox(Prop:items->...->Bishop/Rook...), Panel(Prop:SizeWidth500Height500),Form1(Prop:Text:Chess Piece Moves)
+// add label(Prop:Text:Select a piece from the drop down menu.....), comboBox(Prop:items->...->Bishop/Rook...)(Prop:Appearance->DropDownStyle->DropDownList), Panel(Prop:SizeWidth500Height500),Form1(Prop:Text:Chess Piece Moves)
 
 //--------------------------- C# Chess Board 11 placebuttons ----------------------------------------------------
 //ref link:https://www.youtube.com/watch?v=f3QRYl3o5Ak&list=PLhPyEFL5u-i0YDRW6FLMd1PavZp9RcYdF&index=11
